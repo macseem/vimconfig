62c0d9aba331d71f491661df17a686cc04cefbbd
macseem <macseem@macseem-home-pc.(none)>
2014-01-26 00:47:36 +0200

    create installation script

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ usage() {
 Please view README.md for additional information" 1>&2; 
   exit 1;
 }
-INSTALLPATH="/home/$USER/.vim"
+DEST="/home/$USER/.vim"
+SRC=$(pwd)
+DEPENDENCES=("cscope");
 while getopts "hp:" optname;
 do
 	case "$optname" in
@@ -13,7 +15,8 @@ do
 	  usage
 	  ;;
 	"p") #install path
-	  INSTALLPATH=$OPTARG
+	  DEST=$OPTARG
+	
 	  ;;
 	*)
 	# Соответствий не найдено
@@ -22,4 +25,36 @@ do
 	esac
 done
 
-echo "Install path is set to $INSTALLPATH"
+if ! [ -d $DEST ]; then
+	echo "There is no $DEST."
+	mkdir $DEST
+else
+	let "OLDDIR=$RANDOM * $RANDOM * $RANDOM"
+	OLDDIR="/home/$USER/.$OLDDIR"
+	mv $DEST $OLDDIR 
+	mkdir $DEST
+	mv $OLDDIR $DEST"/.oldvim"
+	echo "Your old .vim dir was moved to $DEST/.oldvim"
+fi
+
+echo "Status: [OK]  Task: Make Directory $DEST" 
+echo "Install path is set to $DEST"
+echo "Start installing"
+
+cp -r $SRC/* $DEST/
+VIMRC="/home/$USER/.vimrc"
+if [ -f $VIMRC ]; then
+	mv "$VIMRC" "$DEST/.oldvimrc"
+	echo "There was .vimrc file in $VIMRC"
+	echo "It was moved to $DEST/.oldvimrc"
+fi
+
+ln -s ${DEST}/vimrc /home/$USER/.vimrc
+echo "Status: [OK]  Task: Create .vimrc symlink"
+
+echo -e "\n\nInstallation Finished"
+echo "Please check if these programs are installed"
+for program in $DEPENDENCES; do
+	echo -e "\t$program"
+done
+echo "Have a good day. Bye ;)"
